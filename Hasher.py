e72375182b35ac1be8f0094096abbf61cd897704
rossirule2012 <rossirule2012@gmail.com>
2014-09-17 20:50:57 +0200

    Removed dehash function

--- Hasher.py
+++ Hasher.py
@@ -9,17 +9,4 @@ def hash(to_hash):
         s+=buffer[len(buffer)-1-i]
     return s
 
-def dehash(to_dehash):
-    temp=''
-    dehashed=''
-    buffer=[]
-    for char in to_dehash:
-        buffer+=char
-    for i in range(len(buffer)):
-       temp+=buffer[len(buffer)-1-i]
-    for i in range(len(temp)):
-        if i%5==0:
-            dehashed+=(temp[i])
-    return dehashed
-
 
